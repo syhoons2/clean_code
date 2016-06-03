4434d940dacc5d6f08dc4ac0a098ae1c2c930435
syhoons2 <syhoons2@naver.com>
2016-06-03 16:12:57 +0900

    deleteInterface

---
+++ src/com/nts/cleancode/collections/AbstractCollection.java
@@ -0,0 +1,35 @@
+package com.nts.cleancode.collections;
+
+public abstract class AbstractCollection {
+	public void addAll(AbstractCollection c) {
+		if (c instanceof Set) {
+			Set s = (Set)c;
+			for (int i=0; i < s.size(); i++) {
+				if (!contains(s.getElementAt(i))) {
+					add(s.getElementAt(i));
+				}
+			}
+			
+		} else if (c instanceof List) {
+			List l = (List)c;
+			for (int i=0; i < l.size(); i++) {
+				if (!contains(l.get(i))) {
+					add(l.get(i));
+				}
+			}
+		} else if (c instanceof Map) {
+			Map m = (Map)c;
+			for (int i=0; i<m.size(); i++) 
+				add(m.keys[i], m.values[i]);			
+		}
+	}
+	
+	public void add(Object key, Object value) {
+	}
+	
+	public abstract boolean isEmpty();
+	public abstract void add(Object element);
+	public abstract boolean remove(Object element);
+	public abstract boolean contains(Object element);
+	public abstract int size();
+}
